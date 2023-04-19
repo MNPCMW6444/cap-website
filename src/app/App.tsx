f272c192f8eb6158f76f71a74bdc91f923885526
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-19 17:30:29 +0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -125,17 +125,7 @@ const StyledTypography = styled(Typography)`
   color: #fff;
 `;
 
-interface FormData {
-  annualRevenue: string;
-  currency: string;
-  annualGrowthRate: string;
-  currentRunway: string;
-  termLength: number;
-  gracePeriod: string;
-  email: string;
-}
-
-interface ActionStateType {
+ ActionStateType {
   DOING: string;
   IDLE: string;
 }
