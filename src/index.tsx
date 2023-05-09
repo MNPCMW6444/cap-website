5355515880be8b487725b905e25a9723968f4aa7
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-05-09 15:45:48 +0300

    jb

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { WebsiteForm } from "@caphub-group/website-form";
+import WebsiteForm from "@caphub-group/website-form";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
